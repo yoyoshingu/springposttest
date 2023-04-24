cfdeed3da8cdddc1c8deeae48d6fa6c4f7dd3a78
Y Lee <leo@shingu.ac.kr>
2023-04-24 16:48:01 +0900

    project begin

---
+++ src/main/java/com/sg/leo/TpostApplication.java
@@ -0,0 +1,13 @@
+package com.sg.leo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/leo/TpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.leo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
